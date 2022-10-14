9c9d273201439c4c898ba23555d33ba4a6f375e4
samsonalxgit <samsonmanie2022@gmail.com>
2022-10-14 14:50:57 -0700

    2-print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -18,28 +18,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-23 : 42
-#include <stdio.h>
-
-/**
- * main - print alphabets in uppercase
- *
- * Description: using the main function
- * this program prints "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
- * Return: 0
- */
-int main(void)
-{
-	char ch;
-
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		putchar(ch);
-	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
-}
