e950015a0f5ff282bffd173fe64cf083d295ef28
samsonalxgit <samsonmanie2022@gmail.com>
2022-10-13 18:18:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - Entry
+ * Return: Always
+ */
+int main(void)
+{
+	printf("size of a char: %i byte(s)\n", sizeo f(char));
+	printf("size of a char: %i byte(s)\n", sizeo f(int));
+	printf("size of a char: %i byte(s)\n", sizeo f(long int));
+	printf("size of a char: %i byte(s)\n", sizeo f(long long int));
+	printf("size of a char: %i byte(s)\n", sizeo f(float));
+	return (0);
+}
