be0cc1581c5f196a273558b75cb92535b00e7a55
samsonalxgit <samsonmanie2022@gmail.com>
2022-10-17 16:21:29 -0700

    6-size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
  * main - Entry point
  * 
@@ -13,5 +11,4 @@ int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 /**
  * Main - Entry
+ *
  * Return: Always
  */
 int main(void)
 {
-	printf("size of a char: %i byte(s)\n", sizeo f(char));
-	printf("size of a char: %i byte(s)\n", sizeo f(int));
-	printf("size of a char: %i byte(s)\n", sizeo f(long int));
-	printf("size of a char: %i byte(s)\n", sizeo f(long long int));
-	printf("size of a char: %i byte(s)\n", sizeo f(float));
+	printf("size of a char: %1u byte(s)\n", sizeo f(char));
+	printf("size of a int: %1u byte(s)\n", sizeo f(int));
+	printf("size of a long int: %1u byte(s)\n", sizeo f(long int));
+	printf("size of a long long int: %1u byte(s)\n", sizeo f(long long int));
+	printf("size of a float: %1u byte(s)\n", sizeo f(float));
 	return (0);
 }
