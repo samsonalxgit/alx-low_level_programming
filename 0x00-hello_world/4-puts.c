db98331034f345429321b081fd7414c293355071
samsonalxgit <samsonmanie2022@gmail.com>
2022-10-17 15:56:04 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
  * 
@@ -6,7 +8,10 @@
  * 
  * Return: Always 0 (Success)
  */
+
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
