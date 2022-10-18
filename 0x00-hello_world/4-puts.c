347394c17e4eda23d34905cc1ca3b27d4ca379c3
samsonalxgit <samsonmanie2022@gmail.com>
2022-10-18 14:39:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * 
- * Description: 'the program's description'
- * 
- * Return: Always 0 (Success)
+/*
+ * print a line of code using puts
+ *main - print a line of code using puts
+ *
+ * return: 0
  */
-
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
