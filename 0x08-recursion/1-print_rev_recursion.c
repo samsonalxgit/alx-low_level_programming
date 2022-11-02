e2f9e2c36634998093455e422ee1c1f08803070d
samsonalxgit <samsonmanie2022@gmail.com>
2022-11-02 12:47:04 -0700

    String reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - print strings in reverse order
+* @s: string to reverse
+* Return: void
+*/
+
+void _print_rev_recursion(char *s)
+
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
