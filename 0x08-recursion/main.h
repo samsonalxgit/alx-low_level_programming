e6c1830b43f4d9a25332cb8b58149a2a8a786ee8
samsonalxgit <samsonmanie2022@gmail.com>
2022-11-02 12:07:28 -0700

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,18 +1,10 @@
-#include "main.h"
-
-/**
-* _puts_recursion - prints out a string with recursion
-* @s: string to print out
-* Return: void
-*/
-
-void _puts_recursion(char *s)
-{
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
-}
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
