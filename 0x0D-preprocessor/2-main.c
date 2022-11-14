7f5ef0e155364e0451047b18d6f27a3d2b8cbd24
samsonalxgit <samsonmanie2022@gmail.com>
2022-11-14 05:48:19 -0800

    compiled

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** 
+ * main - print out the name of the file being compiled
+ * void nothing
+ * return sucess
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+
+}
