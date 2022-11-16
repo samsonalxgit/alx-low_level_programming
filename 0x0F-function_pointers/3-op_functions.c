3554573390e5830638a57f886f97b55f786eccde
samsonalxgit <samsonmanie2022@gmail.com>
2022-11-16 09:28:45 -0800

    four

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,33 +1,56 @@
-#include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * get_op_func - Program that determines which operator to use
- * @s: Char that deremines what operator function to use
- * Return: Pointer to the appropriate operator function, or NULL if failed
+ * op_add - Function that returns the sum of two values
+ * @a: First argument
+ * @b: Second argument
+ * Return: The sum of two arguments
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	/* ops[6][2]*/
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
-	int i;
-
-	i = 0;
-	while (i < 5)
-	{
-		if (*ops[i].op == *s)
-			return (ops[i].f);
-
-		i++;
-	}
-
-	return (NULL);
+	return (a + b);
 }
+
+/**
+ * op_sub - Function that returns the difference of two values
+ * @a: First argument
+ * @b: Second argument
+ * Return: The difference of two arguments
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function that returns the product of two values
+ * @a: First argument
+ * @b: Second argument
+ * Return: The product of two values
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function that returns the quotient of two values
+ * @a: First argument
+ * @b: Second argument
+ * Return: The quotient of two values
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that returns the remainder of two values
+ * @a: First argument
+ * @b: Second argument
+ * Return: The remainder of two values
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}}
