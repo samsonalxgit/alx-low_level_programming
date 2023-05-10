c871e1486486c0766bb6d403be308dc08e300b99
samsonalxgit <samsonmanie2022@gmail.com>
2023-05-10 13:57:13 -0700

    Linear_search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,7 +10,7 @@ int linear_search(int *array, size_t size, int value)
 	//array---is a pointer to the first element of the array to search in
 	//if value is not present in array or if array is NULL, the function must return -1
 	int index = 0;
-	while (index < size)
+	while(index < size)
 	{
 		if(array[index] = value)
 		{
@@ -22,4 +22,17 @@ int linear_search(int *array, size_t size, int value)
 		}
 	}
 	return -1;
+}
+
+int main(void)
+{
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
